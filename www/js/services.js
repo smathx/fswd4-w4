094da6f1a5a8bb7b8072a61da450e90c5254b2bc
Stephen Matthews <smath@opal7.fsnet.co.uk>
2016-02-28 22:41:00 +0000

    Initial commit

---
+++ www/js/services.js
@@ -0,0 +1,95 @@
+'use strict';
+
+// baseURL is the JSON server URI. It should be 'http://localhost:3000/'
+// when running locally.
+
+angular.module('conFusion.services', ['ngResource'])
+
+.constant("baseURL", "https://fswd4-mobile-smathx.c9users.io:8081/")
+
+.factory('menuFactory', [
+  '$resource', 'baseURL',
+  function ($resource, baseURL) {
+    return $resource(baseURL + "dishes/:id", null, {
+      'update': {
+        method: 'PUT'
+      }
+    });
+  }
+])
+
+.factory('promotionFactory', [
+  '$resource', 'baseURL',
+  function ($resource, baseURL) {
+    return $resource(baseURL + "promotions/:id");
+  }
+])
+
+.factory('corporateFactory', [
+  '$resource', 'baseURL',
+  function($resource, baseURL) {
+    return $resource(baseURL + "leadership/:id");
+  }
+])
+
+.factory('feedbackFactory', [
+  '$resource', 'baseURL',
+  function($resource, baseURL) {
+    return $resource(baseURL + "feedback/:id");
+  }
+])
+
+.factory('favoriteFactory', [
+  '$resource', '$localStorage', 'baseURL',
+  function ($resource, $localStorage, baseURL) {
+
+    var favFac = {};
+    var favorites = [];
+
+    favFac.addToFavorites = function (index) {
+      for (var i = 0; i < favorites.length; i++) {
+        if (favorites[i].id == index)
+          return;
+      }
+      favorites.push({id: index});
+      $localStorage.storeObject('favorites', favorites);
+    };
+
+    favFac.deleteFromFavorites = function (index) {
+      for (var i = 0; i < favorites.length; i++) {
+        if (favorites[i].id == index) {
+          favorites.splice(i, 1);
+          $localStorage.storeObject('favorites', favorites);
+        }
+      }
+    };
+
+    favFac.getFavorites = function () {
+      favorites = $localStorage.getObject('favorites', []);
+      return favorites;
+    };
+
+    return favFac;
+  }
+])
+
+.factory('$localStorage', [
+  '$window',
+  function ($window) {
+    return {
+      store: function (key, value) {
+        $window.localStorage[key] = value;
+      },
+      get: function (key, defaultValue) {
+        return $window.localStorage[key] || defaultValue;
+      },
+      storeObject: function (key, value) {
+        $window.localStorage[key] = JSON.stringify(value);
+      },
+      getObject: function (key, defaultValue) {
+        return JSON.parse($window.localStorage[key] || defaultValue);
+      }
+    };
+  }
+])
+;
